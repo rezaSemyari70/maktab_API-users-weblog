76b731e9a15c6b86d55322737e48c8e7cf1acdf6
reza <rezasemyari1991@gmail.com>
2020-08-05 10:47:53 +0430

    add DefaultLayout

---
+++ src/DefaultLayout/Header.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavbarText,
+    Container
+} from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+const Header = (props) => {
+    const [isOpen,
+        setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Container fluid>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Home</NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <Link className="nav-link"  to="/users">Users</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link  className="nav-link" to="/users">Posts</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link className="nav-link"  to="/users">Todos</Link>
+                        </NavItem>
+                    </Nav>
+                    <NavbarText>JsonBlog</NavbarText>
+                </Collapse>
+            </Navbar>
+        </Container>
+    );
+}
+
+export default Header;
---
+++ src/DefaultLayout/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Footer from './Footer';
+import Header from './Header';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+
+} from "react-router-dom";
+
+
+import { Container } from 'reactstrap';
+function index() {
+    return (
+        <Router>
+            <Header />
+            <Container fluid>
+                <Switch>
+                    <Route path="/users/:id">
+                    </Route>
+                    <Route path="/posts/:id">
+                    </Route>
+                    <Route exact path="/posts">
+                    </Route>
+                    <Route exact path="/users">
+                    </Route>
+                    <Route exact path="/todos">
+                    </Route>
+                    <Route exact path="/">
+                    </Route>
+                </Switch>
+            </Container>
+            <Footer />
+        </Router>
+    )
+}
+
+export default index
